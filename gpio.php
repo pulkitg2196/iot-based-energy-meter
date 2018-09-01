36761acda1a51133cea8f022788885c6ee6b64a2
Pulkit Gaur <32465084+pulkitg2196@users.noreply.github.com>
2018-09-01 20:33:45 +0530

    Update gpio.php

--- gpio.php
+++ gpio.php
@@ -1,7 +1,5 @@
 ﻿<?php
-//TheFreeElectron 2015, http://www.instructables.com/member/TheFreeElectron/
-//This page is requested by the JavaScript, it updates the pin's status and then print it
-//Getting and using values
+
 if (isset ( $_GET["pic"] )) {
 	$pic = strip_tags ($_GET["pic"]);
 	
